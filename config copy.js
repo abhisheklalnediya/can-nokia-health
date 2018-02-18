af6c9bfb6c6df398b474b914180a7a768ab8b341
Abhishek Lal <abhisheklal@Abhisheks-MacBook-Air.local>
2018-02-18 08:58:50 +0530

    config template

---
+++ config copy.js
@@ -0,0 +1,10 @@
+const config = {
+	REQUEST_TOKEN_BASE: "https://developer.health.nokia.com/account/request_token",
+    REQUEST_AUTHORIZATION_BASE: "https://developer.health.nokia.com/account/authorize",
+    REQUEST_ACCESS_TOKEN_BASE: "https://developer.health.nokia.com/account/access_token",
+	
+	KEY: "",
+	SECRET: "",
+}
+
+module.exports = config;
